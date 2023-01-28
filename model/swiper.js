8a245ce94b2104d4b67cef157b8031a9f509fefc
Daes <810203055@qq.com>
2023-01-28 21:59:38 +0800

    feat: 初始化项目

---
+++ model/swiper.js
@@ -0,0 +1,30 @@
+const images = [
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner1.png',
+    text: '1',
+  },
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner2.png',
+    text: '2',
+  },
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner3.png',
+    text: '3',
+  },
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner4.png',
+    text: '4',
+  },
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner5.png',
+    text: '5',
+  },
+  {
+    img: 'https://cdn-we-retail.ym.tencent.com/tsr/home/v2/banner6.png',
+    text: '6',
+  },
+];
+
+export function genSwiperImageList() {
+  return images;
+}
