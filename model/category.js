aeecd331d5382dd1b6e2d412d8658b3c2283d5f3
Daes <810203055@qq.com>
2023-02-07 01:38:52 +0800

    feat: 主页开发

--- model/category.js
+++ model/category.js
@@ -205,62 +205,86 @@ export function getCategoryList() {
   ];
 }
 
-export function getCategoryListData() {
-  return [
-    {
-      groupId: '249480',
-      name: '卫衣',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-1.png',
-    },
-    {
-      groupId: '249480',
-      name: '毛呢外套',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-2.png',
-    },
-    {
-      groupId: '249480',
-      name: '雪纺衫',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-3.png',
-    },
-    {
-      groupId: '249480',
-      name: '羽绒服',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-4.png',
-    },
-    {
-      groupId: '249480',
-      name: '毛衣',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-5.png',
-    },
-    {
-      groupId: '249480',
-      name: '棉衣',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-6.png',
-    },
-    {
-      groupId: '249480',
-      name: '西装',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-7.png',
-    },
-    {
-      groupId: '249480',
-      name: '马甲',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-8.png',
-    },
-    {
-      groupId: '249480',
-      name: '连衣裙',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-9.png',
-    },
-    {
-      groupId: '249480',
-      name: '半身裙',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-10.png',
-    },
-    {
-      groupId: '249480',
-      name: '裤子',
-      thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-11.png',
-    },
-  ].slice(0, 10);
+const cateList = [
+  {
+    groupId: '249480',
+    name: '卫衣',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-1.png',
+  },
+  {
+    groupId: '249480',
+    name: '毛呢外套',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-2.png',
+  },
+  {
+    groupId: '249480',
+    name: '雪纺衫',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-3.png',
+  },
+  {
+    groupId: '249480',
+    name: '羽绒服',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-4.png',
+  },
+  {
+    groupId: '249480',
+    name: '毛衣',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-5.png',
+  },
+  {
+    groupId: '249480',
+    name: '棉衣',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-6.png',
+  },
+  {
+    groupId: '249480',
+    name: '西装',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-7.png',
+  },
+  {
+    groupId: '249480',
+    name: '马甲',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-8.png',
+  },
+  {
+    groupId: '249480',
+    name: '连衣裙',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-9.png',
+  },
+  {
+    groupId: '249480',
+    name: '半身裙',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-10.png',
+  },
+  {
+    groupId: '249480',
+    name: '裤子',
+    thumbnail: 'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-11.png',
+  },
+];
+
+const texts = [
+  'پونكىت',
+  'يېزىق',
+  'ئوتتۇرا',
+  'پونكىت',
+  'يېزىق',
+  'ئوتتۇرا',
+  'پونكىت',
+  'يېزىق',
+  'ئوتتۇرا',
+  'يېزىق',
+];
+
+export function getCategoryListData(sites) {
+  const uly = sites === 'uly';
+  const list = JSON.parse(JSON.stringify(cateList.slice(0, 10)));
+  if (uly) {
+    return list.map((item) => {
+      // eslint-disable-next-line no-param-reassign
+      item.name = texts[Math.floor(Math.random() * 10)];
+      return item;
+    });
+  }
+  return list;
 }
